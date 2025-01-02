ec09d98e3ccb541549db64de72adf04755608f8f
TechEdzprogrammer <113817954+TechEdzprogrammer@users.noreply.github.com>
2025-01-02 18:56:47 +0800

    Final version
    Final version of the project. But will still monitor for possible bugs

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const searchBtn = document.querySelector('.search');
 const firstMeaning = document.querySelector('.meaning1');
 const copyBtn = document.querySelector('.copy');
 const overlay = document.querySelector('.loading-overlay');
+const moreDefCont = document.querySelector('.more-definitions');
+const moreDefBtn = document.querySelector('.more-definitions-btn');
+const appContent = document.querySelector('.app-content');
+let otherMeaning = {};
+let onShowOtherDef = false;
 
 async function getMeaning(){
     try{
@@ -13,9 +18,12 @@ async function getMeaning(){
         loadingScreen();
         const res = await fetchData.json();
         const meaning1 = res[0].meanings[0].definitions[0].definition;
+        otherMeaning = res[0].meanings[0].definitions;
         firstMeaning.value = meaning1;
         copyBtn.classList.remove('copied');
-        copyBtn.value='copy';
+        moreDefCont.classList.remove('more-definitions-hidden');
+        copyBtn.value='Copy';
+
     }
     catch(error){
         OnErrorReload(error);
@@ -50,5 +58,62 @@ function OnErrorReload(errorMsg){
     },1000); 
 }
 
+function resetInputs(){
+    const additionalMeaning = document.querySelectorAll(".other-meaning");
+    const cloneCopy = document.querySelectorAll('.clone-copy');
+    additionalMeaning.forEach(items => items.remove());
+    cloneCopy.forEach(buttons => buttons.remove());
+    moreDefCont.classList.add('more-definitions-hidden');
+    firstMeaning.value = null;
+    copyBtn.classList.remove('copied');
+    copyBtn.value = 'Copy';
+}
+
+moreDefBtn.addEventListener('click', function moreMeaning(meanings){
+    meanings = otherMeaning;
+    meanings.shift();
+    meanings.forEach(otherMeaning = (element, index) =>{
+        let htmlMeaning = `<div class="meaning-container">
+        <textarea type="text" class="text other-meaning other-meaning" id="other-meaning${index}" placeholder="The meaning of the word will be displayed here" readonly>${element.definition}</textarea>
+        </div>
+        <div class="button-container">
+            <input type="button" class="clone-copy" data-textarea="other-meaning${index}" value="Copy">
+        </div>`;
+
+        appContent.insertAdjacentHTML("afterend",htmlMeaning);
+    })
+    onShowOtherDef = true;
+    moreDefCont.classList.add('more-definitions-hidden');
+
+const cloneCopy = document.querySelectorAll('.clone-copy');
+    if(onShowOtherDef){
+        cloneCopy.forEach(function(button){
+            button.addEventListener('click', async function () {
+                const textareaId = this.dataset.textarea;
+                const textarea = document.getElementById(textareaId);
+        
+                if (textarea) {
+                    try {
+                        await navigator.clipboard.writeText(textarea.value); // Modern Clipboard API
+                        this.classList.add('copied');
+                        this.value = 'copied';
+                    } catch (err) {
+                        alert(`${err} cannot be copied`);
+                    }
+                }
+            });
+        });
+    }
+});
+
+word.addEventListener('input', function onChange(event){
+    if(event.inputType === "deleteContentBackward" || event.inputType === "deleteContentForward"){
+        if(word.value === ""){
+            resetInputs();
+        }
+    }
+});
+   
 searchBtn.addEventListener('click', getMeaning);
 copyBtn.addEventListener('click', copyMeaning);
+
